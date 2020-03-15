a4b5bf25bcca772f0c338a951f86e8d6f53cc076
Francesco Burelli <francesco.burelli@gmail.com>
2020-03-15 23:49:46 +0100

    Upload UI improved

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="full-size">
-    <form
-      action="/backend/api/media/"
-      method="post"
-      enctype="multipart/form-data"
-    >
-      <input type="file" name="files" multiple />
-      <input name="upload" value="upload" type="submit" />
-    </form>
+    <div class="wrapper">
+      <form
+        action="/backend/api/media/"
+        method="post"
+        enctype="multipart/form-data"
+      >
+        <input class="full-width" type="file" name="files" multiple />
+        <input name="upload" value="upload" type="submit" />
+      </form>
+    </div>
   </div>
 </template>
 
@@ -44,4 +46,17 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.wrapper form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
+  padding: 16px;
+  text-align: right;
+}
+.wrapper form input {
+  margin: 8px 0px;
+  position: relative;
+}
+</style>
