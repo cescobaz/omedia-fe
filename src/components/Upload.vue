e3429a264722f2cc1fc46a64cab051af71cc6325
Francesco Burelli <francesco.burelli@gmail.com>
2021-03-24 00:54:25 +0100

    upload multipart with javascript and axios

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,26 +1,97 @@
 <template>
   <div class="full-size">
     <div class="wrapper">
-      <form
-        action="/backend/to-import/"
-        method="post"
-        enctype="multipart/form-data"
-      >
-        <input class="full-width" type="file" name="files" multiple />
+      <form @submit.prevent="onSubmit">
+        <input
+          class="full-width"
+          type="file"
+          name="files"
+          multiple
+          @change="onFileChange"
+        />
         <input value="upload" type="submit" />
       </form>
+      <div v-if="!!result">
+        <h2>
+          {{ result.date }}
+        </h2>
+        <table>
+          <tr>
+            <td>imported</td>
+            <td>{{ result.totalOk }}</td>
+          </tr>
+          <tr>
+            <td>not imported</td>
+            <td>{{ result.totalNotImported }}</td>
+          </tr>
+        </table>
+
+        <div>
+          <table>
+            <tr v-for="result in result.results" :key="result.filename">
+              <td>{{ result.contentType }}</td>
+              <td>{{ result.filename }}</td>
+              <td>{{ result.result }}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Upload',
   components: {},
   props: {},
   data () {
     return {
-      section: 'media'
+      section: 'media',
+      files: [],
+      result: {
+        totalOk: 10,
+        totalNotImported: 12,
+        results: [
+          { result: 'ok', filename: 'ciao.jpg', contentType: 'image' },
+          { result: 'ok', filename: 'ciao-2.jpg', contentType: 'image' },
+          {
+            result: 'skipped because exists',
+            filename: 'ciao-4.jpg',
+            contentType: 'image'
+          },
+          { result: 'ok', filename: 'vacanze.mov', contentType: 'video' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onFileChange (event) {
+      console.log('onFileChange', this.files)
+      this.files = event.target.files
+      console.log(this.files)
+    },
+    onSubmit (event) {
+      console.log('onSubmit')
+      const formData = new FormData()
+      this.files.forEach(file => formData.append('files', file))
+      axios
+        .post('backend/to-import/', formData, {
+          headers: { 'content-type': 'multipart/form-data' }
+        })
+        .then(response => {
+          const results = response.data
+          const totalOk = results.filter(result => result.result === 'ok')
+            .length
+          this.result = {
+            date: new Date().toLocaleString(),
+            totalOk,
+            totalNotImported: results.length - totalOk,
+            results
+          }
+        })
+        .catch(console.log)
     }
   }
 }
